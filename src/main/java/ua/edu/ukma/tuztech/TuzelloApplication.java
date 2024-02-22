11cc77ea26ef38cab17e30cbf9c343aa5cbf5b79
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2024-02-22 15:19:25 +0200

    Initialize project

---
+++ src/main/java/ua/edu/ukma/tuztech/TuzelloApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.tuztech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuzelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuzelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/edu/ukma/tuztech/TuzelloApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.tuztech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuzelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
